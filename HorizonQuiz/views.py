7a287c4d8bb7c2086a834034c5e121ee1cd5ea31
meamuri <meammf@yandex.ru>
2016-10-02 10:51:41 +0300

    answer_response now has json format. Friendly for Alamofire

--- HorizonQuiz/views.py
+++ HorizonQuiz/views.py
@@ -10,8 +10,8 @@ def index(request):
 
 def get_answer(request, question_id):
     try:
-        obj = Question.objects.get(pk=question_id)
+        quest_by_id = Question.objects.get(pk=question_id)
     except (KeyError, Question.DoesNotExist):
-        return HttpResponse(str(-1))
+        return HttpResponse(str(-1), content_type="application/json; charset=utf-8")
     else:
-        return HttpResponse(str(obj.true_answer))
+        return HttpResponse(str(quest_by_id.true_answer), content_type="application/json; charset=utf-8")
