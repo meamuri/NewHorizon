8029c22644cfacf06dda76e8716f9afd9137bb89
Roman Dronoff <Roman Dronoff>
2016-10-15 13:00:56 +0100

    check score function was added

--- HorizonQuiz/views.py
+++ HorizonQuiz/views.py
@@ -3,17 +3,26 @@ from django.http import JsonResponse
 import random
 
 
+def check_score(request, value):
+    if 'score' not in request.session:
+        request.session['score'] = 0
+    if value == 0:
+        request.session['score'] += 1
+
+
+def clear_and_return(request, what_you_want_return):
+    del request.session['session_status']
+    del request.session['correct_answer']
+    return JsonResponse({'correct': what_you_want_return})
+
+
 def index(request):
     if 'session_status' not in request.session or request.session['session_status'] != 'we_get_question':
         data = random.choice(Question.objects.all())
         request.session['session_status'] = 'we_get_question'
-        request.session['true_answer'] = data.true_answer
+        request.session['correct_answer'] = data.true_answer
         return JsonResponse(data.serialize())
-
-    # request.session['session_status'] == 'we_get_question':
-    del request.session['session_status']
-    del request.session['true_answer']
-    return JsonResponse({'correct': -1})
+    return clear_and_return(request, -1)
 
 
 def get_answer(request, user_answer):
@@ -21,25 +30,16 @@ def get_answer(request, user_answer):
         return JsonResponse({'correct': 'not in session'})
 
     if request.session['session_status'] != 'we_get_question':
-        del request.session['session_status']
-        del request.session['true_answer']
-        return JsonResponse({
-            'correct': 'not in time',
-            'status is': request.session['session_status']
-        })
+        return clear_and_return(request, 'not in time')
 
-    if 'score' not in request.session:
-        request.session['score'] = 0
-    if request.session['true_answer'] == int(user_answer):
-        request.session['score'] += 1
-
+    check_score(request, request.session['correct_answer'] - int(user_answer))
     res = JsonResponse({
-        'correct': request.session['true_answer'],
+        'correct': request.session['correct_answer'],
         'score': request.session['score']
     })
 
     del request.session['session_status']
-    del request.session['true_answer']
+    del request.session['correct_answer']
     return res
 
 
@@ -47,38 +47,25 @@ def get_accuracy_question(request):
     if 'session_status' not in request.session or request.session['session_status'] != 'get_accuracy_question':
         data = random.choice(AccuracyQuestion.objects.all())
         request.session['session_status'] = 'get_accuracy_question'
-        request.session['true_answer'] = data.true_answer
+        request.session['correct_answer'] = data.true_answer
         return JsonResponse(data.serialize())
-
-    del request.session['session_status']
-    del request.session['true_answer']
-    return JsonResponse({'oops': 'we don\'t know your answer! F5 for new question'})
+    return clear_and_return(request, 'we don\'t know your answer! F5 for new question')
 
 
 def check_accuracy_answer(request, digit_of_answer):
     if 'session_status' not in request.session:
-        return JsonResponse({'correct': 'not in session'})
-
+        return JsonResponse({'delta': 'not in session'})
     if request.session['session_status'] != 'get_accuracy_question':
-        del request.session['session_status']
-        del request.session['true_answer']
-        return JsonResponse({
-            'correct': 'not in time',
-            'status is': request.session['session_status']
-        })
+        return clear_and_return(request, 'not in time')
 
-    if 'score' not in request.session:
-        request.session['score'] = 0
+    delta = abs(request.session['correct_answer'] - int(digit_of_answer))
 
-    delta = abs(request.session['true_answer'] - int(digit_of_answer))
-    if delta == 0:
-        request.session['score'] += 1
+    check_score(request, delta)
 
     res = JsonResponse({
-        'correct': delta,
+        'delta': delta,
         'score': request.session['score']
     })
-
     del request.session['session_status']
-    del request.session['true_answer']
+    del request.session['correct_answer']
     return res
