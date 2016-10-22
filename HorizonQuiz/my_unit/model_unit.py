c65b14e9f8965e31bff3691b8ca359a48fcf8e0a
Meamuri <meammf@yandex.ru>
2016-10-22 14:17:20 +0300

    rename background to map

--- HorizonQuiz/my_unit/model_unit.py
+++ HorizonQuiz/my_unit/model_unit.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Background(models.Model):
+class Map(models.Model):
     url = models.CharField(max_length=256)
     x = models.IntegerField
     y = models.IntegerField
