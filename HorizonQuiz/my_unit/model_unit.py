941043d16520897bbbf8f5eeb37157bdb372f899
Meamuri <meammf@yandex.ru>
2016-10-22 14:29:17 +0300

    map fields with default values were added

--- HorizonQuiz/my_unit/model_unit.py
+++ HorizonQuiz/my_unit/model_unit.py
@@ -2,11 +2,14 @@ from django.db import models
 
 
 class Map(models.Model):
-    url = models.CharField(max_length=256)
-    x = models.IntegerField
-    y = models.IntegerField
-    width = models.IntegerField
-    height = models.IntegerField
+    url = models.CharField(max_length=256, default='default')
+    x = models.IntegerField(default=0)
+    y = models.IntegerField(default=0)
+    width = models.IntegerField(default=100)
+    height = models.IntegerField(default=100)
+
+    def __str__(self):
+        return self.url + ' x: ' + str(self.x) + ' y: ' + str(self.y)
 
 
 class Region(models.Model):
