28f8d2afbc1c5dfe52882275f4ecd42521901f57
Roman Dronoff <Roman Dronoff>
2016-11-05 12:53:01 +0100

    server can change game-move between players

--- HorizonQuiz/my_unit/game_logic.py
+++ HorizonQuiz/my_unit/game_logic.py
@@ -1,8 +1,31 @@
 players = []         # players id
+enemies = dict()     # player_id ---> player_id (his enemy)
 game_ids = dict()    # player_id ---> game_id
 game_round = dict()  # game_id ---> step of game
-game_turn = dict()   # game_id ---> TURN_OF_GAME[i]
+game_turn = dict()   # game_id ---> TURN_OF_GAME[k]
 maps = dict()        #
 whose_step_in_game = dict()
 
-TURN_OF_GAME = ('started', 'wait_question', 'want_check_answer', 'finished')
+TURN_OF_GAME = {
+    'started': 0,
+    'attack_some_area': 1,
+    'attack_neutral_area': 7,
+    'attack_enemy_area': 8,
+    'enemy_attack_me!!!': 9,
+    'check_fight_result': 2,
+    'accuracy_fight': 3,
+    'check_accuracy': 4,
+    'round_is_over': 5,
+    'finished': 6,
+}
+
+# 1. бой за нейтральную
+# 2. бой за вражескую:
+#   2.1. Оба получили вопросы
+#   2.2. Оба ответ:
+#       2.2.1. Оба ответили неверно
+#       2.2.2. Атакующий ответил неверно
+#       2.2.3. Защищающийся ответил неверно
+#       2.2.4. Оба ответили верно:
+#           2.2.4.1. Вопрос на точность
+#
--- HorizonQuiz/views.py
+++ HorizonQuiz/views.py
@@ -22,7 +22,7 @@ def get_accuracy_answer(request, digit_of_answer):
     return views_unit.user_want_take_answer(request, views_unit.WE_GET_ACCURACY_QUESTION, int(digit_of_answer))
 
 
-def check_and_resume_game(game_id):
+def round_up_or_resume(game_id):
     game_logic.game_round[game_id] += 1
     if game_logic.game_round[game_id] == 15:
         game_logic.game_turn = game_logic.TURN_OF_GAME[3]
@@ -34,34 +34,39 @@ def check_and_resume_game(game_id):
 
 
 def game_center(request, num=1):
-    key = request.session.session_key
-    if key not in game_logic.game_ids:
+    player_key = request.session.session_key
+    if player_key not in game_logic.game_ids:
         return JsonResponse({'error': 'the game is not initialized!'})
 
-    current_game = game_logic.game_ids[key]  # ищем игру по id сессии
-    if game_logic.game_turn[current_game] == game_logic.TURN_OF_GAME[2]:
+    current_game = game_logic.game_ids[player_key]  # ищем игру по id сессии
+
+    if game_logic.game_turn[current_game] == game_logic.TURN_OF_GAME['check_fight_result']:
         res = fight_result(request, int(num))
-        check_and_resume_game(current_game)
+        round_up_or_resume(current_game)
+        game_logic.whose_step_in_game[current_game] = game_logic.enemies[player_key]
         return JsonResponse(res)
 
-    return attack_area(request, int(num), key, current_game)
-
-
-def attack_area(request, id_area, player_key, current_game):
     # возможно, игрок player_key делает ход не в свой шаг
     # чтобы это проверить, ищем в словаре whose_step айди игрока, чей ход. Ищем по ключу игры
     if player_key != game_logic.whose_step_in_game[current_game]:
         return JsonResponse({'error': 'step is not your!'})
 
+    return attack_area(request, int(num), player_key, current_game)
+
+
+def attack_area(request, id_area, player_key, current_game):
     # Если игрок делает ход в свой ход, проверяем, что атакуемая область нейтральна или
     # занята врагом (не является занятой им же)
     if game_logic.maps[current_game][id_area] == player_key:
         return JsonResponse({'error': 'is your area!'})
 
-    # Если все хорошо, получаем вопрос
     request.session['area_id'] = id_area
-    game_logic.game_turn[current_game] = game_logic.TURN_OF_GAME[2]
-    return get_enum_question(request)
+    if game_logic.maps[current_game][id_area] == -1:  # область нейтраьная
+        game_logic.game_turn[current_game] = game_logic.TURN_OF_GAME['attack_neutral_area']
+        return get_enum_question(request)
+
+    game_logic.game_turn[game_logic.enemies[player_key]] = game_logic.TURN_OF_GAME['enemy_attack_me!!!']
+    game_logic.game_turn[current_game] = game_logic.TURN_OF_GAME['attack_enemy_area']
 
 
 def fight_result(request, user_answer):
@@ -82,15 +87,17 @@ def fight_result(request, user_answer):
 def player_start_game(request, width=1, height=1, map_id=1):
     game_map = get_play_map(request, int(map_id), int(width), int(height))
 
-    if len(game_logic.players) == 0:  # если новый игрок единственный, кто ожидающий игру, добавляем его в очередь
-        game_logic.players.append(request.session.session_key)
+    player_key = request.session.session_key
+    if len(game_logic.players) == 0:  # or game_logic.players[0] == player_key:
+        # если новый игрок единственный, кто ожидающий игру, добавляем его в очередь
+        game_logic.players.append(player_key)
         return game_map
 
     enemy = game_logic.players[0]  # за врага принимаем первого в очереди
     game_logic.queue_of_gamers = game_logic.players[1:]  # и удаляем его из очереди
 
     game_id = uuid.uuid1()  # случайную уникальную комбинацию принимаем за игровой id
-    init_game(request.session.session_key, enemy, game_id, int(map_id))  # инициализация карты, соперников, стадии игры
+    init_game(player_key, enemy, game_id, int(map_id))  # инициализация карты, соперников, стадии игры
 
     return game_map
 
@@ -108,7 +115,7 @@ def get_play_map(request, map_id, width, height):
         'region-images': lst_urls,
         'region-poses': lst_pos,
         'region-areas': lst_areas,
-        'region_sizes': lst_sizes
+        'region-sizes': lst_sizes
     })
 
 
@@ -121,10 +128,12 @@ def init_game(player_key, his_enemy, game_id, map_id):
     # его соперник, возможно, уже отчаялся ждать и отложил устройство
     game_logic.whose_step_in_game[game_id] = player_key
 
+    game_logic.enemies[player_key] = his_enemy
+    game_logic.enemies[his_enemy] = player_key
     game_logic.game_ids[player_key] = game_id  # id сессии нового игрока присваиваем словарю игровых сессий
     game_logic.game_ids[his_enemy] = game_id  # id сессии его врага присваиваем словарю игровых сессий
     game_logic.game_round[game_id] = 0
-    game_logic.game_turn[game_id] = game_logic.TURN_OF_GAME[0]  # стадия игры -- самое начало
+    game_logic.game_turn[game_id] = game_logic.TURN_OF_GAME['started']
 
     current_map = []
     views_unit.fill_game_map(current_map, map_id, player_key, his_enemy)  # распределяем локации на карте
