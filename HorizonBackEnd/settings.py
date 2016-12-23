a3a4fec66847ea94afbb5fc14b528352cfce1e2a
meamuri <meammf@yandex.ru>
2016-12-23 19:26:31 +0300

    move local settings to another file

--- HorizonBackEnd/settings.py
+++ HorizonBackEnd/settings.py
@@ -11,27 +11,17 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+try:
+    from .my_settings import *
+except ImportError:
+    pass
 
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vpwt%jzdw_-l7^^!v5bai(suf8&9)7fxx(e%c^dw18j!%9^h()'
-
-
-PUSH_NOTIFICATIONS_SETTINGS = {
-        "GCM_API_KEY": "[your api key]",
-        "APNS_CERTIFICATE": "/path/to/your/certificate.pem",
-        "WNS_PACKAGE_SECURITY_ID": "[your package security id, e.g: 'ms-app://e-3-4-6234...']",
-        "WNS_SECRET_KEY": "[your app secret key, e.g.: 'KDiejnLKDUWodsjmewuSZkk']",
-}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -53,11 +43,6 @@ INSTALLED_APPS = [
 
     "push_notifications",
 ]
-
-try:
-    from local_settings import *
-except ImportError:
-    pass
 
 
 MIDDLEWARE = [
@@ -89,17 +74,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'HorizonBackEnd.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
